b0674ed75d2254561a51eda3e23a68eae73e7247
luskax02-beep <luskax02@gmail.com>
2026-06-12 15:25:21 -0400

    feat: make Statsig SDK optional
    Prevent application crash and add a warning when VITE_STATSIG_CLIENT_KEY is missing by making the StatsigProvider conditional.

--- index.tsx
+++ index.tsx
@@ -9,13 +9,23 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-const statsigClientKey = import.meta.env.VITE_STATSIG_CLIENT_KEY || 'client-key-placeholder';
+const statsigClientKey = import.meta.env.VITE_STATSIG_CLIENT_KEY;
 
 const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <StatsigProvider sdkKey={statsigClientKey} waitForInitialization={false} user={{}}>
+
+if (statsigClientKey) {
+  root.render(
+    <React.StrictMode>
+      <StatsigProvider sdkKey={statsigClientKey} waitForInitialization={false} user={{}}>
+        <App />
+      </StatsigProvider>
+    </React.StrictMode>
+  );
+} else {
+  console.warn("VITE_STATSIG_CLIENT_KEY is not set. StatsigProvider will not be loaded.");
+  root.render(
+    <React.StrictMode>
       <App />
-    </StatsigProvider>
-  </React.StrictMode>
-);
+    </React.StrictMode>
+  );
+}
